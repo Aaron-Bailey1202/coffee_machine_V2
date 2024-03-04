6bf57b38a98de4f6d4d21820f9432d1c16c83e27
Aaron Bailey <97994050+Aaron-Bailey1202@users.noreply.github.com>
2024-03-04 21:48:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,12 @@ machine_on = True
 
 while machine_on:
     drink_order = input(f"What would you like? {menu.get_items()}")
-    order = menu.find_drink(drink_order)
     if drink_order == "report":
         coffee_machine.report()
         cash_register.report()
     elif drink_order == "off":
         break
     else:
-        if not coffee_machine.is_resource_sufficient(order):
-            break
-        else:
-            cash_register.make_payment(order.cost)
+        order = menu.find_drink(drink_order)
+        if coffee_machine.is_resource_sufficient(order) and cash_register.make_payment(order.cost):
             coffee_machine.make_coffee(order)
